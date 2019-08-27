e1662a44cf3291e9ad37aa48198b54c8b63e3a21
inagaraj <nagaraj2794@rediffmail.com>
2019-08-27 11:53:38 +0530

    updated signup and sign slide form design

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() { 
+    $("#signUp").click(function() { 
+        $("#container").addClass("right-panel-active");
+    });
+    $("#signIn").click(function() { 
+        $("#container").removeClass("right-panel-active");
+    }); 
+});
